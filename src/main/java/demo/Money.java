81da88c5b9cb6c1c8073a9ca78e061a2aea9cb2f
qthouver <qthouver@enssat.fr>
2024-09-10 18:07:48 +0200

    Ajout du Test

--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,5 +1,7 @@
 package demo;
 
+import java.util.Objects;
+
 public class Money {
     private int amount;
     private String currency;
@@ -9,11 +11,29 @@ public class Money {
         this.amount = amount;
     }
 
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public Money add(Money m2){
+        Money m1 = new Money(this.amount + m2.getAmount(), this.currency);
+        return m1;
+    }
+
     @Override
-    public String toString() {
-        return "Money{" +
-                "amount=" + amount +
-                ", currency='" + currency + '\'' +
-                '}';
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount && Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
     }
 }
