d65315aec5a2a46ab89dd23463e7aa900c035cf9
qthouver <qthouver@enssat.fr>
2024-09-10 17:24:44 +0200

    Premier commit

---
+++ src/main/java/demo/Money.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Money {
+    private int amount;
+    private String currency;
+
+    public Money(int amount, String currency){
+        this.currency = currency;
+        this.amount = amount;
+    }
+}
